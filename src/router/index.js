5bbf3c39b7c670b41acf83fe03787285b1a87faa
Sulaiman <sulaimandvlpr@example.com>
2026-06-12 15:58:57 +0700

    Configure GitHub Actions deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router' // 1. DIUBAH DI SINI
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL), // 2. DIUBAH DI SINI
   scrollBehavior() {
     return { top: 0 }
   },
